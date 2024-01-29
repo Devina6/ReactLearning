bb372423402393e087daa7239956fbf6b817147e
Devina <work.devina@gmail.com>
2024-01-29 13:06:06 +0530

    without JSX

--- react-project/src/components/hello.js
+++ react-project/src/components/hello.js
@@ -1,11 +1,8 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello D</h1>
-        </div>
-    )
+    return React.createElement('div', {id:'hello',className:'dummy'},
+        React.createElement('h1',null, 'Hello D'))
 }
 
 export default Hello
