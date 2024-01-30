c7b22b4e127ee342e3edb8b47865f65ab8c2d21b
Devina <work.devina@gmail.com>
2024-01-30 10:46:11 +0530

    conditonal rendering - ternary operator

--- react-project/src/components/userGreeting.js
+++ react-project/src/components/userGreeting.js
@@ -10,14 +10,9 @@ class userGreeting extends Component {
       }
     }
   render() {
-
-    let message 
-    if(this.state.isLoggedIn){
-        message = <div>Welcome Devina</div>
-    }else{
-        message = <div> Welcome Guest </div>
-    }
-    return <div>{message}</div>
+    return(
+        this.state.isLoggedIn ? <div>Welcome Devina</div> : <div> Welcome Guest </div>
+    )
   }
 }
 
